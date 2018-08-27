30ae3cd54880cd0db3a39c048cc559cd23750128
Simon Bazilian <simon@artery.net>
2018-08-27 21:07:46 +0200

    Clean-up

--- src/App.js
+++ src/App.js
@@ -2,16 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-let defaultTextColor = '#FFD333';
 let defaultStyle = {
-  color: defaultTextColor
+  color: '#FFD333'
 };
 
 class Aggregate extends Component {
   render () {
     return (
       <div style={{...defaultStyle, width: "40%", display: 'inline-block'}}>
-        <h2 style={{color: defaultTextColor}}>Number Text</h2>
+        <h2 style={{defaultStyle}}>Number Text</h2>
       </div>
     );
   }
@@ -44,7 +43,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Title</h1>
+        <h1 style={{...defaultStyle, 'font-size': '54px'}}>Title</h1>
         <Aggregate/>
         <Aggregate/>
         <Filter/>
